0de690a9a0d3c6e35d22b4f066cb36639fdf2b8a
Julie-ANANI <julie.anani@outlook.com>
2021-05-04 19:22:59 +0200

    add folder front of branch develop

---
+++ front/src/index.js
@@ -0,0 +1,25 @@
+// == Import : npm
+import React from 'react';
+
+
+// ReactDOM is a package that provides DOM specific methods that can be used at the top level of a web app to enable an efficient way of managing DOM elements of the web page. ReactDOM provides the developers with an API containing the following methods and a few more. 
+
+// render()
+// findDOMNode()
+// unmountComponentAtNode()
+// hydrate()
+// createPortal()
+import { render } from 'react-dom';
+
+// == Import : local
+// Composants
+import App from 'src/components/App';
+
+// == Render
+// 1. Élément React racine (celui qui contient l'ensemble de l'app)
+//    => crée une structure d'objets imbriqués (DOM virtuel)
+const rootReactElement = <App />;
+// 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
+const target = document.getElementById('root');
+// 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
+render(rootReactElement, target);
