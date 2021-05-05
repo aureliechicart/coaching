93a4acc6631895b99334cc4f5dfe45ecc820c6c7
Julie-ANANI <julie.anani@outlook.com>
2021-05-05 19:49:50 +0200

    modif file index.js

--- server/app/router.js
+++ server/app/router.js
@@ -5,16 +5,6 @@ const router = Router();
 const themeController = require('./controllers/themeController');
 const Theme = require('./models/theme');
 
-
-
-router.get('/', (_,res)=>{
-    res.send("Hello World !");
-});
-
-router.get('/madou', (_,res)=>{
-    res.send("Hello Madou !");
-});
-
 /**
  * Returns all themes from the database
  * @route GET /themes
@@ -27,7 +17,7 @@ router.get('/themes', themeController.getAllThemes);
  * Returns a theme from the database with its id
  * @route GET /themes/{id}
  * @group Themes
- * @returns {Array<Theme>} 200 - An array of one them
+ * @returns {<Theme>} 200 - An instance of one theme
  */
 router.get('/themes/:id', themeController.getOneTheme);
 
