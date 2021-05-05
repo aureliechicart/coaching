fdb7fefcb6655fb5093d027d2d2952aaf0b48077
mabakayaro <madineba@gmail.com>
2021-05-05 15:13:26 +0200

    Header + general progress bar done

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -6,40 +6,17 @@ import axios from 'axios';
 // == Import
 import './styles.css';
 
-import Header from 'src/components/Header';
+// Import from Semantic UI
+ 
+// import Header from 'src/components/Header';
+import ThemeDashboard from 'src/components/ThemeDashboard';
 
 // == Composant
 const App = () => { 
 
-const [title, setTitle] = useState('');
-
-  const loadTitle = () => {
-
-  // envoyer une requête à l'API
-  axios.get('http://localhost:3000/v1/madou')
-  .then((response) => {
-    // exécuté quand la réponse arrive, si la réponse est un succès
-
-    // on stocke dans le state (dans la case pour les articles) les articles récupérés
-    setTitle(response.data);
-  })
-  .catch((error) => {
-    // exécuté quand la réponse arrive, si la réponse est un échec
-    console.log(error);
-
-    // TODO il faudrait afficher l'information à l'utilisateur
-  })
-  }
-
-
-  useEffect(() => {
-    loadTitle();
-  }, []);
-
-
 return(
   <div className="app">
-    <Header title={title}></Header>
+      <ThemeDashboard></ThemeDashboard>
   </div>
 );}
 
