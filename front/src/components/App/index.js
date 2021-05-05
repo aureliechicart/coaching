4cbf5a3608fea4f614086a69dce87e61c91d12bf
mabakayaro <madineba@gmail.com>
2021-05-05 10:51:50 +0200

    jonction créée entre front et back

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,17 +1,47 @@
 // == Import npm
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// bibliothèque pour faciliter les appels AJAX (en utilisant des Promise)
+import axios from 'axios';
 
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
 
+import Header from 'src/components/Header';
+
 // == Composant
-const App = () => (
+const App = () => { 
+
+const [title, setTitle] = useState('');
+
+  const loadTitle = () => {
+
+  // envoyer une requête à l'API
+  axios.get('http://localhost:3000/v1/madou')
+  .then((response) => {
+    // exécuté quand la réponse arrive, si la réponse est un succès
+
+    // on stocke dans le state (dans la case pour les articles) les articles récupérés
+    setTitle(response.data);
+  })
+  .catch((error) => {
+    // exécuté quand la réponse arrive, si la réponse est un échec
+    console.log(error);
+
+    // TODO il faudrait afficher l'information à l'utilisateur
+  })
+  }
+
+
+  useEffect(() => {
+    loadTitle();
+  }, []);
+
+
+return(
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <Header title={title}></Header>
   </div>
-);
+);}
 
 // == Export
 export default App;
