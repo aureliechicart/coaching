8d371d488a13582b0c830bc6871cc6e71d2590fe
Julie-ANANI <julie.anani@outlook.com>
2021-05-05 18:38:30 +0200

    add controller theme/ theme/:id

--- server/app/router.js
+++ server/app/router.js
@@ -2,13 +2,33 @@ const { Router } = require('express');
 
 const router = Router();
 
+const themeController = require('./controllers/themeController');
+const Theme = require('./models/theme');
 
-router.get('/', (req,res)=>{
+
+
+router.get('/', (_,res)=>{
     res.send("Hello World !");
 });
 
-router.get('/madou', (req,res)=>{
+router.get('/madou', (_,res)=>{
     res.send("Hello Madou !");
 });
+
+/**
+ * Returns all themes from the database
+ * @route GET /themes
+ * @group Themes
+ * @returns {Array<Themes>} 200 - An array of themes
+ */
+router.get('/themes', themeController.getAllThemes);
+
+/**
+ * Returns a theme from the database with its id
+ * @route GET /themes/{id}
+ * @group Themes
+ * @returns {Array<Theme>} 200 - An array of one them
+ */
+router.get('/themes/:id', themeController.getOneTheme);
 
 module.exports = router;
