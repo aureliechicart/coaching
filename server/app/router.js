047b671eb2ac1c114a78ee96675b87546ce0d322
Julie-ANANI <julie.anani@outlook.com>
2021-05-06 16:17:06 +0200

    modification file import.sql

--- server/app/router.js
+++ server/app/router.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 
 const themeController = require('./controllers/themeController');
 const missionController = require('./controllers/missionController');
+const userController = require('./controllers/userController');
 
 const router = Router();
 
@@ -30,11 +31,30 @@ router.get('/themes/:id', themeController.getOneTheme);
 router.get('/themes/:id/missions/:id', missionController.getOneMission);
 
 /**
- * Returns all themes from the database
+ * Returns all missions from the database
  * @route GET /themes/{id}/missions
  * @group The Missions
  * @returns {Array<Missions} 200 - An array of missions
  */
 router.get('/themes/:id/missions', missionController.getAllMissions);
+
+/**
+ * Returns a user from the database with its id
+ * @route GET /users/{id}
+ * @group Users
+ * @returns {<User>} 200 - An instance of one user
+ */
+router.get('/users/:id', userController.getOneUser);
+
+
+/**
+ * Returns all users from the database
+ * @route GET /users
+ * @group The users
+ * @returns {Array<Themes>} 200 - An array of user
+ */
+router.get('/users', userController.getAllusers);
+
+
 
 module.exports = router;
