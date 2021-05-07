d2f5d73b4030f79209fdb3347a61fa4dc3586a61
lucasquill <chouilloulucas@gmail.com>
2021-05-07 13:48:01 +0200

    accordion ok with server

--- server/app/database.js
+++ server/app/database.js
@@ -1,15 +1,13 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
 
 // For heroku deployment
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
-
-// For development
 const pool = new Pool({
+connectionString: process.env.DATABASE_URL,
+ssl: {
+rejectUnauthorized: false
+ 	}
 });
 
 module.exports = pool;
