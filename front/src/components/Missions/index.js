6cb3b18062701c2bdc1fb733d509df8d5b9bdb4d
lucasquill <chouilloulucas@gmail.com>
2021-05-07 13:39:10 +0200

    accordion add with style

--- front/src/components/Missions/index.js
+++ front/src/components/Missions/index.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {  Divider } from 'semantic-ui-react';
 import './style.css';
 import ThemeProgressBar from 'src/components/ThemeProgressBar';
-import Accordion from 'src/components/Accordion';
+import AccordionComponent from 'src/components/AccordionComponent';
+import axios from 'axios'
 
-const Missions = () => (
-  <div className="missions">
-    <h1>titre du projet</h1>
-    <ThemeProgressBar />
-    <Divider />
-    <Accordion />
-  </div>
-)
+const Missions = () => {
+  const [missions, setMissions] = useState([]);
+
+  const loadMissions = () => {
+    axios.get('http://localhost:3000/v1/api/themes/1/missions')
+      .then((response) => {
+        setMissions(response.data);
+      })
+      .catch((error) => {
+        // exécuté quand la réponse arrive, si la réponse est un échec
+        console.log(error);
+
+        // TODO il faudrait afficher l'information à l'utilisateur
+      })
+
+  };
+  useEffect(() => {
+    loadMissions();
+  }, []);
+
+  return (
+    <div className="missions">
+      <h1>titre du projet</h1>
+      <ThemeProgressBar />
+      <Divider />
+      <AccordionComponent missions={missions}/>
+    </div>
+  )
+}
 
 export default Missions
