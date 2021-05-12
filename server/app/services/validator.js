bf6172cbf4dd35361ce055ac9f599cd2f410a4b6
MahanoraTetuanui <mtcmtahiti@gmail.com>
2021-05-12 16:31:43 +0200

    add jsdoc

--- server/app/services/validator.js
+++ server/app/services/validator.js
@@ -3,17 +3,24 @@
 const validor = {
     /**
      * This function will validate or not the data's body
-     * @param {Schema} schema data of the body
+     * @param {Schema} schema - data of the body
      * @return - a message error if there is a problem with the data's schema or nothing (the application will continue)
      */
     validateBody : (schema) => (req, res, next) => {
-            
-
+        
         const { error } = schema.validate(req.body);
-        console.log(error);
+        /**
+         * If there is an error, a message with the precise data who is not conform with the schema
+         */
         if (!error) {
+            /**
+             * Nothing, the app continue
+             */
             next();
         } else {
+            /**
+             * A data is not conform  with the schema
+             */
             const { message, path }= error.details[0];
             res.status(400).json({message, path});
         };
