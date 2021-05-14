af26d3341dd9172963e8c9079105c0688f199417
Julie-ANANI <julie.anani@outlook.com>
2021-05-14 20:30:33 +0200

    test route again

--- server/app/controllers/testController.js
+++ server/app/controllers/testController.js
@@ -7,41 +7,63 @@ const testController = {
     /**
     * Route POST /v1/api/login
     */
-   searchArray: async (req, res) => {
+   searchArray2: async (req, res) => {
   
-    let auth_token = process.env.EXTERNAL_API_KEY
-    console.log(auth_token);
+//     let auth_token = process.env.EXTERNAL_API_KEY
+//     console.log(auth_token);
 
-    const form = new FormData();
-    // form.append('x-auth', auth_token);
+//     // const form = new FormData();
+//     // form.append('x-auth', auth_token);
     
-     console.log(req.body);
+//      console.log(req.body);
    
-    try {
-        if(req.body){
-        let searchUser;
-        const rtest = await fetch(`${process.env.EXTERNAL_API_BASE_URL}/api/${process.env.ENDPOINT_PROMOS}`, {
-            method: 'POST',
-            body : form ,
-            headers: {
-                // 'Content-Type': 'application/json',
-                'authorization': `Bearer ${auth_token}`
-              }
-    }).then(res => console.log (res))
-    .then(res.json())
-          .then(json => searchUser = json);
-          console.log(res.headers.get('content-type'));
-  console.log(rtest);
-}
-  res.status(200).json({
-    status: 'succes',
-    data: req.body,
-  })
+//     try {
+//         if(req.body){
+//         let searchUser;
+//         const rtest = await fetch(`${process.env.EXTERNAL_API_BASE_URL}/api/${process.env.ENDPOINT_PROMOS}`, {
+//             method: 'POST',
+//             headers: {
+//                 'authorization': `Bearer ${auth_token}`
+//               }
+//     }).then(res => console.log (res))
+//     .then(res.json())
+//           .then(json => searchUser = json);
+//           console.log(res.headers.get('content-type'));
+//   console.log(rtest);
+// }
+//   res.status(200).json({
+//     status: 'succes',
+//     data: req.body,
+//   })
 
-    } catch (err) {
-        res.status(500).json(err.message);
-    };
+//     } catch (err) {
+//         res.status(500).json(err.message);
+//     };
 
+},
+
+
+searchArray: async (req, res)=> {
+       let auth_token = process.env.EXTERNAL_API_KEY
+        //     console.log(auth_token);
+        const rtest = await fetch(`${process.env.EXTERNAL_API_BASE_URL}/api/cohorts`, {
+        method: 'POST',
+        headers: {
+            'authorization': `Bearer ${auth_token}`
+          }} )
+        
+          if (!res) {
+              return res.status(400).json({
+                  status: 'error',
+                  error : 'req body cannot be empty',
+              });
+          } else {
+    
+        console.log(rtest);
+        for (const key in rtest) {console.log(key)}
+          res.status(200).json(rtest);
+          console.log(req.body);
+        }
 },
 
 };
