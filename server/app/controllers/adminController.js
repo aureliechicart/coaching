df5b1752460143a9b1dbde554445d0dae2093c03
aureliechicart <aurelie.chicart@gmail.com>
2021-05-15 16:08:33 +0200

    updating addAdmin with isTeacher check

--- server/app/controllers/adminController.js
+++ server/app/controllers/adminController.js
@@ -1,7 +1,6 @@
 const User = require('../models/user');
 const fetch = require('node-fetch');
 const FormData = require('form-data');
-const interactController = require('./interactController');
 
 const adminController = {
     /**
@@ -16,7 +15,7 @@ const adminController = {
 
         // We look it up in the external api to identify its external api user id
         try {
-            let apiUser;
+            let emailUser;
             await fetch(`${process.env.EXTERNAL_API_BASE_URL}/api/check_email`, {
                 method: 'POST',
                 body: form,
@@ -24,34 +23,58 @@ const adminController = {
                     'X-AUTH-TOKEN': process.env.EXTERNAL_API_KEY
                 }
             }).then(res => res.json())
-                .then(json => apiUser = json);
-            console.log(apiUser);
+                .then(json => emailUser = json);
+            console.log(emailUser);
 
-            if (!apiUser.success) {
-                res.status(404).json(apiUser.message);
+            if (!emailUser.success) {
+                res.status(404).json(emailUser.message);
             } else {
-                // we search the external api user in our database
-                const theInternalUser = await User.checkByApiId(apiUser.data.id);
-                
-                // If no user is found in our database, it means the user is connecting for the first time to our app
-                // we create a new record in our user table
-                if (!theInternalUser) {
-                    // We create a new user with admin status true
-                    const theNewUser = await new User({ api_user: apiUser.data.id, admin_status: true });
-                    await theNewUser.save();
-                    res.status(200).json(theNewUser);
-
-                } else {
-                    // if the user is found in our database
-                    // we update its admin_status to true if not already true
-                    if (!theInternalUser.admin_status) {
-                        theInternalUser.admin_status = true;
+                //    we search the user in the O'Clock API to confirm this user has the teacher role
+                let apiUser;
+                await fetch(`${process.env.EXTERNAL_API_BASE_URL}/api/user/${emailUser.data.id}`, {
+                    method: 'GET',
+                    headers: {
+                        'X-AUTH-TOKEN': process.env.EXTERNAL_API_KEY
                     }
-                    await theInternalUser.save();
-                    res.status(200).json(theInternalUser);
+                }).then(res => res.json())
+                    .then(json => apiUser = json);
+                console.log(apiUser.data.is_teacher);
+
+                if (!apiUser.success) {
+                    res.status(404).json(apiUser.message);
+                } else {
+                    // if the user doesn't have the teacher role
+                    if (!apiUser.data.is_teacher) {
+                        res.status(401).json(`This user doesnt have a teacher role in the O'Clock API`);
+                    } else {
+                        // the user has the teacher role: we check if they exist in our database yet
+                        const theInternalUser = await User.checkByApiId(apiUser.data.id);
+
+                        // If no user is found in our database, it means the user is connecting for the first time to our app
+                        // we create a new record in our user table
+                        if (!theInternalUser) {
+                            // We create a new user with admin status true
+                            const theNewUser = await new User({ api_user: apiUser.data.id, admin_status: true });
+                            await theNewUser.save();
+                            console.log(theNewUser);
+                            apiUser.oap_id = theNewUser.id;
+                            apiUser.oap_admin_status = theNewUser.admin_status;
+
+                        } else {
+                            // if the user is found in our database
+                            // we update its admin_status to true if not already true
+                            if (!theInternalUser.admin_status) {
+                                theInternalUser.admin_status = true;
+                            }
+                            await theInternalUser.save();
+                            apiUser.oap_id = theInternalUser.id;
+                            apiUser.oap_admin_status = theInternalUser.admin_status;
+                        }
+                        // Now the user has been created/updated withe admin status, we return the full apiUser object
+                        res.status(200).json(apiUser);
+                    }
                 }
             }
-
 
         } catch (err) {
             res.status(500).json(err.message);
