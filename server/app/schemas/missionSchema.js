be3621c87e8ae6925a4f7ea3c9625603350a9eda
MahanoraTetuanui <mtcmtahiti@gmail.com>
2021-05-18 13:52:35 +0200

    update message for all schema

--- server/app/schemas/missionSchema.js
+++ server/app/schemas/missionSchema.js
@@ -4,22 +4,22 @@ const schema = {
     newMission : Joi.object({
         title : Joi.string().required().messages({
             'string.empty': `Malgré vos grands pouvoirs Jedi,un titre tu dois saisir !`,
-            'string.base': `Attention au côté obscure, chaine de caractère pour le titre tu dois saisir !`
+            'string.base': `Attention o'coaching obscure Jedi, une chaîne de caractère pour le titre tu dois saisir !`
         }),
         advice: Joi.string().allow(null, '').messages({
-            'string.base': `Attention au côté obscure, chaine de caractère pour le conseil tu dois saisir !`}),
+            'string.base': `Attention o'coaching obscure Jedi, une chaîne de caractère pour le conseil tu dois saisir !`}),
         position: Joi.number().allow(null, '').integer().min(0).allow(null, '').messages({
-            'string.base': `Attention au côté obscure, nombre pour la description tu dois saisir !`
+            'string.base': `Attention o'coaching obscure, un nombre pour la position tu dois saisir !`
         })
     }),
 
     updateMission : Joi.object({
         title : Joi.string().allow(null, '').messages({
-            'string.base': `Attention au côté obscure, chaine de caractère pour le titre tu dois saisir !`}),
+            'string.base': `Attention o'coaching obscure Jedi, une chaîne de caractère pour le titre tu dois saisir !`}),
         advice: Joi.string().allow(null, '').messages({
-            'string.base': `Attention au côté obscure, chaine de caractère pour le conseil tu dois saisir !`}),
+            'string.base': `Attention o'coaching obscure Jedi, une chaîne de caractère pour le conseil tu dois saisir !`}),
         position: Joi.number().integer().min(0).allow(null, '').messages({
-            'string.base': `Attention au côté obscure, nombre pour la description tu dois saisir !`
+            'string.base': `Attention o'coaching obscure Jedi, nombre pour la position tu dois saisir !`
         })
     })
 
--- server/app/schemas/themeSchema.js
+++ server/app/schemas/themeSchema.js
@@ -4,23 +4,23 @@ const schema = {
     newTheme : Joi.object({
         title : Joi.string().required().messages({
             'string.empty': `Malgré vos grands pouvoirs Jedi, un titre tu dois saisir !`,
-            'string.base': `Attention au côté obscure, chaine de caractère pour le titre tu dois saisir !`
+            'string.base': `Attention o'coaching obscure Jedi, une chaîne de caractère pour le titre tu dois saisir !`
         }),
         description: Joi.string().allow(null, '').messages({
-            'string.base': `Attention au côté obscure, chaine de caractère pour la description tu dois saisir !`
+            'string.base': `Attention o'coaching obscure Jedi, une chaîne de caractère pour la description tu dois saisir !`
         }),
         position: Joi.number().integer().min(0).allow(null, '').messages({
-            'string.base': `Attention au côté obscure, nombre pour la description tu dois saisir !`
+            'string.base': `Attention o'coaching obscure Jedi, un nombre pour la position tu dois saisir !`
         }),
     }),
 
     updateTheme : Joi.object({
         title: Joi.string().allow(null, '').messages({
-            'string.base': `Attention au côté obscure, chaine de caractère pour le titre tu dois saisir !`}),
+            'string.base': `Attention o'coaching obscure Jedi, une chaîne de caractère pour le titre tu dois saisir !`}),
         description: Joi.string().allow(null, '').messages({
-            'string.base': `Attention au côté obscure, chaine de caractère pour la description tu dois saisir !`}),
+            'string.base': `Attention au o'coaching obscure Jedi, une chaîne de caractère pour la description tu dois saisir !`}),
         position: Joi.number().integer().min(0).allow(null, '').messages({
-            'string.base': `Attention au côté obscure, nombre pour la description tu dois saisir !`
+            'string.base': `Attention o'coaching obscure Jedi, un nombre pour la position tu dois saisir !`
         })
     })
 
