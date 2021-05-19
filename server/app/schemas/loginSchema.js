67f91898dbc6421b906b4ac00fa8cefa7d971492
MahanoraTetuanui <74900662+MahanoraTetuanui@users.noreply.github.com>
2021-05-19 15:02:39 +0200

    Update loginSchema.js

--- server/app/schemas/loginSchema.js
+++ server/app/schemas/loginSchema.js
@@ -4,12 +4,12 @@ const schema = {
     newLogin : Joi.object({
         login_email : Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr', 'io'] } }).required().messages({
             'string.email': `Oyez oyez veuillez mettre un e-mail ou password valide !`,
-            'string.empty': `Si rejoindre le coaching de la force est votre volonté, il faut saisir de ton e-mail !`,
+            'string.empty': `Si rejoindre le coaching de la force est votre volonté, il faut saisir ton e-mail !`,
             'string.base': `Vérifier ton e-mail ou ton password tu devras ! `,
             'any.required': `Oups l'e-mail est important pour rejoindre le coaching de la force !`
         }),
         login_password : Joi.string().required().messages({
-            'string.empty': `Si rejoindre le coaching de la force est votre volonté, il faut saisir de ton password !`,
+            'string.empty': `Si rejoindre le coaching de la force est votre volonté, il faut saisir ton password !`,
             'any.required': `Oups le password est important pour rejoindre le coaching de la force !`,
             'string.base': `Vérifier ton e-mail ou ton password tu devras ! `
         }),
