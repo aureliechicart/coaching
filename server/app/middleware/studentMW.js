012c08e8dc7863e14336d19a0d81f08d010d9e92
Aurelie Chicart <61583641+aureliechicart@users.noreply.github.com>
2021-05-19 13:51:02 +0200

    Update studentMW.js

--- server/app/middleware/studentMW.js
+++ server/app/middleware/studentMW.js
@@ -3,14 +3,14 @@
 const studentMW = (req, res, next) => {
     if (!req.session.user) {
         res.status(401).json(`L'utilisateur doit être connecté pour faire cette requête`);
-        return
-    }
+        return;
+    };
 
     // if the user is connected but doesn't have the student role, we send the message that the access is forbidden
     if (!req.session.user.is_student) {
         res.status(403).json(`L'utilisateur doit avoir le rôle d'étudiant O'Clock pour faire cette requête`);
         return;
-    }
+    };
 
     //the user is connected and is allowed to execute the controller method 
     next();
