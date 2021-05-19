71178f495e016d341ec12024689b90109c1b1221
Julie-ANANI <julie.anani@outlook.com>
2021-05-19 12:22:32 +0200

    add rows.length model in interact

--- server/app/models/interact.js
+++ server/app/models/interact.js
@@ -83,7 +83,7 @@ class Interact {
         WHERE interact.user_id = $1
         ;`, [userId]);
 
-        if (rows) {
+        if (rows.length) {
             return rows.map(row => new Interact(row));
         } else {
             throw new NoInteractError();
